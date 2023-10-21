6268878e205f1de6d0bd4b519cb734a9ce2d744d
Александр Филипенко <ALEXANDER.FILIPENKO2014@YANDEX.RU>
2023-10-22 01:30:14 +0500

    foodgram

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -16,7 +16,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-USE_SQLITE = os.getenv('USE_DB', 'False') == 'True'
+USE_SQLITE = os.getenv('USE_DB', 'False')
+# == 'True'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 

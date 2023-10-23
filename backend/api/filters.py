199847cc8747c38fa492274b14a66e2c5e1f2fca
Александр Филипенко <ALEXANDER.FILIPENKO2014@YANDEX.RU>
2023-10-23 16:33:28 +0500

    foodgram

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -15,26 +15,24 @@ class IngredientSearchFilter(SearchFilter):
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all(),
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    is_favorited = filters.BooleanFilter(method='is_favorited_filter')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='is_in_shopping_cart_filter'
     )
-    is_in_shopping_cart = filters.NumberFilter(
-        method='filter_is_in_shopping_cart')
-    is_favorited = filters.NumberFilter(
-        method='filter_is_favorited')
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+        fields = ('tags', 'author')
 
-    def filter_is_favorited(self, queryset, name, value):
-        if self.request.user.is_authenticated and value:
-            return queryset.filter(favorite__author=self.request.user)
+    def is_favorited_filter(self, queryset, name, value):
+        user = self.request.user.pk
+        if value:
+            return queryset.filter(favorite__user=user)
         return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, name, value):
-        if self.request.user.is_authenticated and value:
-            return queryset.filter(shopping_cart__author=self.request.user)
+    def is_in_shopping_cart_filter(self, queryset, name, value):
+        user = self.request.user.pk
+        if value:
+            return queryset.filter(shoppingcart__user=user)
         return queryset

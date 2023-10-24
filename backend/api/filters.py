a5a5dcec6d636bf3670ba4632e0618849e491c9f
Александр Филипенко <ALEXANDER.FILIPENKO2014@YANDEX.RU>
2023-10-24 18:07:08 +0500

    foodgram

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django_filters import rest_framework as filters
-from django_filters.rest_framework import filters
+# from django_filters.rest_framework import filters
 
 # from rest_framework.filters import SearchFilter
 # FilterSet

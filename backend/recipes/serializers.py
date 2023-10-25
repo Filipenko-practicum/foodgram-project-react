dac89003fb98b767495e7cde0b2a176bb532e6cd
Александр Филипенко <ALEXANDER.FILIPENKO2014@YANDEX.RU>
2023-10-25 19:32:36 +0500

    foodgram

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,6 +1,5 @@
 from django.forms import ValidationError
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers
 from rest_framework.serializers import (
     IntegerField,
     ModelSerializer,

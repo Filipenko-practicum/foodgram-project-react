d458767250eb78f36829c021eb8f4c04a9643980
Александр Филипенко <ALEXANDER.FILIPENKO2014@YANDEX.RU>
2023-10-26 00:12:46 +0500

    foodgram

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -31,19 +31,17 @@ class RecipeFilter(filters.FilterSet):
         fields = (
             'tags',
             'author',
-            'is_favorited',
-            'is_in_shoppingcart',
         )
 
     def filter_is_favorited(self, queryset, name, value):
         if value and not self.request.user.is_anonymous:
-            return queryset.filter(is_favorited__user=self.request.user)
+            return queryset.filter(favorite__user=self.request.user)
         return queryset
 
     def filter_is_in_shoppingcart(self, queryset, name, value):
         if value and not self.request.user.is_anonymous:
 
-            return queryset.filter(is_in_shopping_cart__user=self.request.user)
+            return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
 
 

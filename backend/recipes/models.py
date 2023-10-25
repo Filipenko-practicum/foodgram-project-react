1560e184ca8222f426613d080efe3e9450ccc12a
Александр Филипенко <ALEXANDER.FILIPENKO2014@YANDEX.RU>
2023-10-25 18:20:31 +0500

    foodgram

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -220,6 +220,6 @@ class ShoppingCart(UserRelation):
     """
 
     class Meta(UserRelation.Meta):
-        default_related_name = 'shopping_cart'
+        default_related_name = 'shoppingcart'
         verbose_name = 'Корзина'
         verbose_name_plural = 'Корзина'

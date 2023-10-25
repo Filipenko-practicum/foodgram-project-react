63f06015492a31d51fc888a8ca20f3340514655c
Александр Филипенко <ALEXANDER.FILIPENKO2014@YANDEX.RU>
2023-10-25 17:58:26 +0500

    foodgram

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,7 +1,6 @@
 from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Ingredient, Recipe
 
-from django_filters.rest_framework import filters
+from recipes.models import Ingredient, Recipe
 
 
 class IngredientSearchFilter(FilterSet):

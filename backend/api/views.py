79a97669461324759efcf6cadd041a2349927e6a
Александр Филипенко <ALEXANDER.FILIPENKO2014@YANDEX.RU>
2023-11-07 20:00:20 +0500

    foodgram 2

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,6 @@ from rest_framework.decorators import action
 from rest_framework.permissions import (
     AllowAny,
     IsAuthenticated,
-    IsAuthenticatedOrReadOnly,
 )
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet

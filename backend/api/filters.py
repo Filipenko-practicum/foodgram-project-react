3396da0c5211cec94b244f1541bfa6e457e0248e
Александр Филипенко <ALEXANDER.FILIPENKO2014@YANDEX.RU>
2023-11-09 22:27:02 +0500

    foodgram 2

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -22,7 +22,7 @@ class RecipeFilter(filters.FilterSet):
         method='filter_is_in_shopping_cart'
     )
 
-    def filter_is_favorited(self, queryset,name, value):
+    def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
             return queryset.filter(favorite__user=self.request.user)
         return queryset

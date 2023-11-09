bbed96ff38549164345c0884ef78d9114d3ec9af
Александр Филипенко <ALEXANDER.FILIPENKO2014@YANDEX.RU>
2023-11-09 23:25:39 +0500

    foodgram 2

--- backend/api/views.py
+++ backend/api/views.py
@@ -193,7 +193,7 @@ class UserViewSet(NewUserViewSet):
         return self.get_paginated_response(
             SubscribedSerializer(
                 self.paginate_queryset(
-                    User.objects.filter(subscriber__following=request.user)
+                    User.objects.filter(following_subscriber=request.user)
                 ),
                 many=True,
                 context={'request': request},
